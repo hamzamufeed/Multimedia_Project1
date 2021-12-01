1d54e6d4f0c3e303fe3f797404c6e0295c9934a9
hamzamufeed <68274995+hamzamufeed@users.noreply.github.com>
2021-12-01 18:43:59 +0200

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from "@angular/core";
+import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {MainComponent} from "./main/main.component";
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: '/welcome', component: HomeComponent },
+  { path: '/main', component: MainComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
